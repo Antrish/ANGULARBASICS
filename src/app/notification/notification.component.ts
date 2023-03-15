2cbb7815c74fbb7bc0032198452b2a942b1b35a2
Antrish <antrish.mishra14@gmail.com>
2023-03-15 17:21:17 +0530

    ngClass, removed Hiiden property binding

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class="alert alert-success" [hidden]="ShowNotification">
+  template: `<div class="alert alert-success" [ngClass]="{fadeOut:ShowNotification}">
     This site keeps cookies for better user experience.
     <div class="close"><button class="btn" (click)="closeNotification()">X</button></div>
   </div>`,
-  styles: ['.notification-div{ font-family:italic; background-color:green}', '.close{float:right; margin-top:-15px}', 'p{font-family:bold; text-align:center}']
+  styles: ['.notification-div{ font-family:italic; background-color:green}',
+           '.close{float:right; margin-top:-15px}', 'p{font-family:bold; text-align:center}',
+           '.fadeOut {visibility: hidden; opacity: 0; transition: visibility 0s 2s, opacity 2s linear;}']
 })
 export class NotificationComponent {
   ShowNotification:boolean=false;
@@ -15,3 +17,5 @@ export class NotificationComponent {
   }
 
 }
+
+// background-color:red; opacity:0; transition: visibility 0 10s, opacity 10s linear;
